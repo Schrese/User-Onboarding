9791184b530748fcf9e7983c88c4600112e858ba
Schrese <53787633+Schrese@users.noreply.github.com>
2019-10-17 19:50:48 -0400

    cannot get submit button to work yet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactDom from 'react-dom';
 import MyForm from './components/myForm.js';
 
 function App() {
--- src/components/myForm.js
+++ src/components/myForm.js
@@ -1,9 +1,16 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {withFormik, Form, Field} from 'formik';
-import Axios from 'axios';
+import axios from 'axios';
+import * as Yup from 'yup';
 
-const MyForm = () => {
-    const [newUser, setNewUser] = useState();
+
+
+const MyForm = ({setShowForm, values, status, errors, touched}) => {
+    const [newUser, setNewUser] = useState([]);
+
+    useEffect(() => {
+        status && setNewUser (newUser => [...newUser, status])
+    },[status])
 
     return (
         <div>Hi Again
@@ -11,20 +18,35 @@ const MyForm = () => {
                 <Field 
                     type = 'text' 
                     name = 'name' 
-                    placeholder = 'name'/>
+                    placeholder = 'name'
+                    // value = {values.name}
+                    // onChange = {handleChange}
+                    />
+                    {touched.name && errors.name && (
+                        <p>{errors.name}</p>
+                    )}
+                    
                 <Field 
                     type = 'text' 
                     name = 'email' 
-                    placeholder = 'email'/>
+                    placeholder = 'email'
+                    // value = {values.email}
+                    // onChange = {handleChange}
+                    />
                 <Field 
                     type = 'password' 
                     name = 'password' 
-                    placeholder = 'password'/>
+                    placeholder = 'password'
+                    // value = {values.password}
+                    // onChange = {handleChange}
+                    />
 
                 <label>Agree to our Terms:
                     <Field 
                         type = 'checkbox' 
-                        name = 'TOS' />
+                        name = 'TOS' 
+                        checked = {values.TOS}
+                        />
                 </label>
                 <button type = 'submit'>Submit</button> 
 
@@ -35,14 +57,23 @@ const MyForm = () => {
     )
 }
 const FormikMyForm = withFormik({
-    mapPropsToValues({name, email, password, TOS}) {
+    mapPropsToValues({ name, email, password, TOS }) {
         return {
             name: name || '',
             email: email || '',
             password: password || '',
             TOS: TOS || false
-        }
-    }
+        };
+    },
+    handleSubmit(values, {setStatus}) {
+        axios.post('https://reqres.in/api/users', values)
+            .then(res => {setStatus(res.data);})
+            .catch(err => console.log('what have you done?', err.response));
+    },
+    validationSchema: Yup.object().shape({
+        name: Yup.string(3).required('Need a Longer Name'),
+        email: Yup.string().required()
+    })
     
 })(MyForm);
 
